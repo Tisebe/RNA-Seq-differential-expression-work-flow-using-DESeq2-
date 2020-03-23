d54be29058ea73abc71d09b3622d7879051b8595
unknown <Tony Isebe@DESKTOP-GLBF5HG>
2020-03-23 17:27:17 +0300

    Further changes on DESEQ analysis.

--- DESEQ.R
+++ DESEQ.R
@@ -40,3 +40,78 @@ bamFiles
 library( "Rsamtools" )
 seqinfo( BamFile( bamFiles[1] ) )
 
+#Counting reads in genes
+
+#To count how many read map to each gene, we need transcript annotation. Download the current GTF file with human gene annotation from Ensembl.
+
+#From this file, the function makeTranscriptDbFromGFF from the GenomicFeatures package constructs a database of all annotated transcripts.
+
+#For this lab you can use the truncated version of this file, called Homo_sapiens.GRCh37.75.subset.gtf.gz.
+
+#Preparing gene models from GFF files
+
+library( "GenomicFeatures" )
+
+
+hse <- makeTxDbFromGFF("Homo_sapiens.GRCh37.75.subset.gtf", format="gtf")
+#Extract exons for each gene
+exonsByGene <- exonsBy( hse, by="gene" )
+exonsByGene
+
+#Reads counting
+
+#The function summarizeOverlaps from the GenomicAlignments package will do this. 
+
+library( "GenomicAlignments" )
+se <- summarizeOverlaps( exonsByGene, BamFileList( bamFiles ), mode="Union",
+                         singleEnd=FALSE, ignore.strand=TRUE, fragments=TRUE )
+
+se
+
+#We use the counting mode "Union", which indicates that those reads which overlap any portion of exactly one feature are counted. As this experiment produced paired-end reads, we specify singleEnd =FALSE. As protocol was not strand-specific, we specify ignore.strand = TRUE. fragments = TRUE indicates that we also want to count reads with unmapped pairs. This last argument is only for use with paired-end experiments.
+
+# Details on how to read from the BAM files can be specified using the BamFileList function. For example, to control the memory, we could have specified that batches of 2 000 000 reads should be read at a time: 
+
+BamFileList( bamFiles, yieldSize = 2000000 )
+
+#SummarizedExperiment object : Output of counting
+
+#We investigate the resulting SummarizedExperiment class by looking at the counts in the assay slot, the phenotypic data about the samples in colData slot (in this case an empty DataFrame), and the data about the genes in the rowData slot.
+
+# Count data
+head( assay(se) )
+
+#Column sum
+colSums( assay(se) )
+
+#Phenotypic data
+colData(se)
+
+#Data about genes
+rowData(se)
+
+#The colData slot, so far empty, should contain all the meta data. We hence assign our sample table to it:
+
+#Column data
+colData(se) <- DataFrame( sampleTable )
+
+#We can extract columns from the colData using the $ operator, and we can omit the colData to avoid extra keystrokes.
+
+colData(se) <- DataFrame( sampleTable )
+#We can extract columns from the colData using the $ operator, and we can omit the colData to avoid extra keystrokes.
+
+#Column data
+colData(se) <- DataFrame( sampleTable )
+
+#We can extract columns from the colData using the $ operator, and we can omit the colData to avoid extra keystrokes.
+
+colData(se)$treatment
+se$treatment
+
+#We can also use the sampleName table to name the columns of our data matrix:
+
+colnames(se) <- sampleTable$sampleName
+head( assay(se) )
+
+#This SummarizedExperiment object se is then all we need to start our analysis.
+
